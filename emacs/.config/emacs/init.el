9a2540d61374c7fc2a06b07521c45f07c583c846
Sthyr <122413261+Sthyr@users.noreply.github.com>
2026-04-26 13:10:28 -0400

    Initial emacs config

---
+++ emacs/.config/emacs/init.el
@@ -0,0 +1,4 @@
+(setq custom-file "~/.config/emacs/emacs.custom") ; Settings defined with the menu are saved here, not in my init.el
+
+(column-number-mode -1) ;; Show the number in the modeline 
+(display-line-numbers-mode 1) ;; Show line number in buffer, prefix with global- to show numbers on evert buffer
